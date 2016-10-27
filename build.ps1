7e9cdb58543e550f376126faead9bb47befdc73a
Alex Ellis <alexellis2@gmail.com>
2016-10-27 16:11:48 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
 Remove-Item GuidGenerator\\bin -Recurse 
-docker rm guid-generator-assets-1
 docker build --build-arg HTTP_PROXY=%HTTP_PROXY% --build-arg HTTPS_PROXY=%HTTPS_PROXY% -f build.Dockerfile -t guid-generator-assets .
 
 docker create --name guid-generator-assets-1 guid-generator-assets
 
 docker cp guid-generator-assets-1:C:\\build\\GuidGenerator\\bin .\\GuidGenerator\bin
+docker rm guid-generator-assets-1
 
 docker build -f Dockerfile -t guid-generator:0.1 .
 
